f7db5f4fd5804374886162fa6ac47a34719bf4e7
chaima-khalil <chaimakhalil38@gmail.com>
2023-05-09 11:05:00 +0100

    configuration DATABASE_URL

--- src/Controller/SongController.php
+++ src/Controller/SongController.php
@@ -1,10 +1,13 @@
 <?php
+
 namespace App\Controller;
+
 use Psr\Log\LoggerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+
 class SongController extends AbstractController
 {
     #[Route('/api/songs/{id<\d+>}', methods: ['GET'], name: 'api_songs_get_one')]
@@ -16,9 +19,11 @@ class SongController extends AbstractController
             'name' => 'Waterfalls',
             'url' => 'https://symfonycasts.s3.amazonaws.com/sample.mp3',
         ];
+
         $logger->info('Returning API response for song {song}', [
             'song' => $id,
         ]);
+
         return $this->json($song);
     }
 }
--- src/Controller/VinylController.php
+++ src/Controller/VinylController.php
@@ -2,7 +2,7 @@
 
 namespace App\Controller;
 
-use Psr\Cache\CacheItemInterface;
+use App\Service\MixRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -31,16 +31,11 @@ class VinylController extends AbstractController
     }
 
     #[Route('/browse/{slug}', name: 'app_browse')]
-    public function browse(HttpClientInterface $httpClient, CacheInterface $cache, string $slug = null): Response
+    public function browse(HttpClientInterface $httpClient, CacheInterface $cache, MixRepository $mixRepository, string $slug = null): Response
     {
         $genre = $slug ? u(str_replace('-', ' ', $slug))->title(true) : null;
 
-        $mixes = $cache->get('mixes_data', function(CacheItemInterface $cacheItem) use ($httpClient) {
-            $cacheItem->expiresAfter(5);
-            $response = $httpClient->request('GET', 'https://raw.githubusercontent.com/SymfonyCasts/vinyl-mixes/main/mixes.json');
-
-            return $response->toArray();
-        });
+        $mixes = $mixRepository->findAll();
 
         return $this->render('vinyl/browse.html.twig', [
             'genre' => $genre,
@@ -48,3 +43,4 @@ class VinylController extends AbstractController
         ]);
     }
 }
+
